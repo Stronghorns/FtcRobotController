c5f565b7bbd150c72456a302007a6e62fae93ca2
Stronghorns <strongernights@gmail.com>
2023-03-03 06:49:32 -0800

    Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/ScanCone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/ScanCone.java
@@ -29,6 +29,13 @@ import org.openftc.apriltag.AprilTagDetection;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 import org.openftc.easyopencv.OpenCvInternalCamera;
 
 import java.util.ArrayList;
@@ -38,7 +45,12 @@ public class ScanCone extends LinearOpMode
 {
     OpenCvCamera camera;
     AprilTagDetectionPipeline aprilTagDetectionPipeline;
-
+    DcMotor left_back;
+    DcMotor left_front;
+    DcMotor right_back;
+    DcMotor right_front;
+    DcMotor linear_slide;
+    Servo hook_servo;
     static final double FEET_PER_METER = 3.28084;
 
     // Lens intrinsics
@@ -49,6 +61,8 @@ public class ScanCone extends LinearOpMode
     double fy = 578.272;
     double cx = 402.145;
     double cy = 221.506;
+    double linearSlide;
+    int minHeight = 50;
 
     // UNITS ARE METERS
     double tagsize = 0.166;
@@ -61,8 +75,7 @@ public class ScanCone extends LinearOpMode
     AprilTagDetection tagOfInterest = null;
 
     @Override
-    public void runOpMode()
-    {
+    public void runOpMode() throws InterruptedException {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
         aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
@@ -167,12 +180,40 @@ public class ScanCone extends LinearOpMode
         }
 
         /* Actually do something useful */
-        if (tagOfInterest.id == Left) {
+        left_back = hardwareMap.get(DcMotor.class, "left_back");
+        left_front = hardwareMap.get(DcMotor.class, "left_front");
+        right_back = hardwareMap.get(DcMotor.class, "right_back");
+        right_front = hardwareMap.get(DcMotor.class, "right_front");
+        right_front.setDirection(DcMotorSimple.Direction.REVERSE);
+        right_back.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        } else if (tagOfInterest == null || tagOfInterest.id == Middle) {
+        {
+            moveMotors(1);
+        }
+        if (tagOfInterest == null) {
+            hook_servo.setPosition(1);
+            strafeMotors(1);
+            Thread.sleep(1000);
+            moveMotors(0);
+        }
+        else if (tagOfInterest.id == Left) {
+            hook_servo.setPosition(1);
+            moveSlide(0.5);
+            turnMotors(-1);
+            Thread.sleep(300);
+            stopSlide();
+            moveMotors(0);
 
+        } else if (tagOfInterest.id == Middle) {
+            hook_servo.setPosition(1);
+            moveMotors(1);
+            Thread.sleep(1000);
+            moveMotors(0);
         } else if (tagOfInterest.id == Right) {
-
+            hook_servo.setPosition(1);
+            turnMotors(1);
+            Thread.sleep(300);
+            moveMotors(0);
         }
         /* You wouldn't have this in your autonomous, this is just to prevent the sample from ending */
         while (opModeIsActive()) {sleep(20);}
@@ -187,5 +228,36 @@ public class ScanCone extends LinearOpMode
         telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", Math.toDegrees(detection.pose.yaw)));
         telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", Math.toDegrees(detection.pose.pitch)));
         telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
+    }
+
+    void moveMotors(double power) {
+        left_back.setPower(-power);
+        left_front.setPower(-power);
+        right_back.setPower(-power);
+        right_front.setPower(-power);
+    }
+
+    void turnMotors(double power) {
+        left_back.setPower(-power);
+        left_front.setPower(-power);
+        right_back.setPower(power);
+        right_front.setPower(power);
+    }
+
+    void strafeMotors(double power) {
+        left_back.setPower(-power);
+        left_front.setPower(power);
+        right_back.setPower(power);
+        right_front.setPower(-power);
+    }
+
+    void moveSlide(double power) {
+        linear_slide.setPower(-power);
+        linearSlide = linear_slide.getCurrentPosition();
+    }
+    void stopSlide() {
+        if (linear_slide.getCurrentPosition() != linearSlide && !(linear_slide.getCurrentPosition() > minHeight)) {
+            linear_slide.setPower(1 - linearSlide / linear_slide.getCurrentPosition());
+        }
     }
 }
